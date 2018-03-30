30edfe17ffd0d6949a4abf26c61cdf918de0ed26
unknown <you@domain.com>
2018-03-30 14:21:14 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/example/coolweather/db/province.java
@@ -0,0 +1,38 @@
+package com.example.coolweather.db;
+
+/**
+ * Created by CL on 2018/3/30.
+ */
+
+public class province {
+
+    private  int id;
+
+    private  String provinceName;
+
+    private  int provinceCode;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id=id;
+    }
+
+    public String getProvinceName(){
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName){
+        this.provinceName=provinceName;
+    }
+
+    public  int getProvinceCode(){
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode=provinceCode;
+    }
+}
