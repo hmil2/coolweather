d04c5211118f14df7c217a238777ddd57a825b84
unknown <you@domain.com>
2018-04-02 12:05:58 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by CL on 2018/4/2.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
---
+++ app/src/main/java/com/example/coolweather/gson/Basic.java
@@ -0,0 +1,23 @@
+package com.example.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by CL on 2018/4/2.
+ */
+
+public class Basic {
+
+    @SerializedName("city")
+    public String cityName;
+
+    @SerializedName("id")
+    public String weatherId;
+
+    public Update update;
+
+    public class Update{
+        @SerializedName("loc")
+        public String updateTime;
+    }
+}
